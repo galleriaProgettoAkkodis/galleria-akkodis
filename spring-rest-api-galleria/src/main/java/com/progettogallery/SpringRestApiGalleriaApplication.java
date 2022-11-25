ebd7e449c890198149c4b0cd803ff8e295b18325
dagasp <dagasp10@gmail.com>
2022-11-25 10:46:36 +0100

    primo commit

---
+++ spring-rest-api-galleria/src/main/java/com/progettogallery/SpringRestApiGalleriaApplication.java
@@ -0,0 +1,13 @@
+package com.progettogallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiGalleriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApiGalleriaApplication.class, args);
+	}
+
+}
---
+++ spring-rest-api-galleria/src/main/java/com/progettogallery/entities/Immagine.java
@@ -0,0 +1,8 @@
+package com.progettogallery.entities;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Immagine {
+
+}
